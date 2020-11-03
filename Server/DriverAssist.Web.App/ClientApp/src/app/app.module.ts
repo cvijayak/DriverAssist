f707869faec012e716f9713bd16708181270b10d
Chandrasekar Vijayakumar <chandrasekar.vijayak@outlook.com>
2020-11-03 20:05:16 +0530

    Added client application

--- Server/DriverAssist.Web.App/ClientApp/src/app/app.module.ts
+++ Server/DriverAssist.Web.App/ClientApp/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
@@ -14,7 +14,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
+    DashboardComponent,
     CounterComponent,
     FetchDataComponent
   ],
@@ -23,7 +23,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: '', component: DashboardComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
